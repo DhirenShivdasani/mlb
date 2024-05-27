5600d3ab03ef6fada737d0093a173b71781f9660
Dhiren Shivdasani <dhiren3102@gmail.com>
2024-05-26 21:37:41 -0400

    changes

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ def get_merged_data():
     return jsonify(merged_data.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
