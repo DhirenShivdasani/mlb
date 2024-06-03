359b922670cc9fe78b03f13b51ead6db231b24ef
Dhiren Shivdasani <dhiren3102@gmail.com>
2024-06-03 18:37:05 -0400

    '6/3'

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_cors import CORS
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import asyncio
 import websockets
 import threading
@@ -12,11 +12,9 @@ CORS(app)
 connected_clients = set()
 
 async def notify_clients():
-    while True:
-        await asyncio.sleep(10)  # Adjust this as necessary
-        if connected_clients:  # asyncio.wait doesn't accept an empty list
-            message = "update"
-            await asyncio.gather(*(client.send(message) for client in connected_clients))
+    if connected_clients:
+        message = "update"
+        await asyncio.gather(*(client.send(message) for client in connected_clients))
 
 async def register(websocket):
     connected_clients.add(websocket)
@@ -33,11 +31,12 @@ def start_websocket_server(port):
     asyncio.set_event_loop(loop)
     start_server = websockets.serve(websocket_handler, "0.0.0.0", port)
     loop.run_until_complete(start_server)
-    loop.create_task(notify_clients())
     loop.run_forever()
 
-@app.route('/merged_data')
+@app.route('/merged_data', methods=['GET', 'POST'])
 def get_merged_data():
+    if request.method == 'POST':
+        threading.Thread(target=asyncio.run, args=(notify_clients(),)).start()
     try:
         merged_data = pd.read_csv('merged_data.csv')
         return jsonify(merged_data.to_dict(orient='records'))
