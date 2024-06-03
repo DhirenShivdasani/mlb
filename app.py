cabfc316c5af5f3e17417d592da5f6fb7070aff9
Dhiren Shivdasani <dhiren3102@gmail.com>
2024-06-03 18:33:03 -0400

    '6/3'

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import asyncio
 import websockets
 import threading
 import pandas as pd
+import os
+
 app = Flask(__name__)
 CORS(app)
 
@@ -14,7 +16,7 @@ async def notify_clients():
         await asyncio.sleep(10)  # Adjust this as necessary
         if connected_clients:  # asyncio.wait doesn't accept an empty list
             message = "update"
-            await asyncio.wait([client.send(message) for client in connected_clients])
+            await asyncio.gather(*(client.send(message) for client in connected_clients))
 
 async def register(websocket):
     connected_clients.add(websocket)
@@ -26,19 +28,23 @@ async def register(websocket):
 async def websocket_handler(websocket, path):
     await register(websocket)
 
-def start_websocket_server():
+def start_websocket_server(port):
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    start_server = websockets.serve(websocket_handler, "0.0.0.0", 5000)
+    start_server = websockets.serve(websocket_handler, "0.0.0.0", port)
     loop.run_until_complete(start_server)
+    loop.create_task(notify_clients())
     loop.run_forever()
-merged_data = pd.read_csv('merged_data.csv')
 
 @app.route('/merged_data')
 def get_merged_data():
-    return jsonify(merged_data.to_dict(orient='records'))
+    try:
+        merged_data = pd.read_csv('merged_data.csv')
+        return jsonify(merged_data.to_dict(orient='records'))
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    threading.Thread(target=start_websocket_server).start()
-
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    threading.Thread(target=start_websocket_server, args=(port,)).start()
+    app.run(debug=True, use_reloader=False, port=port)
