6ab36632d002d1e78645b1fd344071706ff01f9a
Dhiren Shivdasani <dhiren3102@gmail.com>
2024-06-13 17:10:04 -0400

    'c'

--- merged_data.py
+++ merged_data.py
@@ -25,45 +25,6 @@ def upload_to_aws(local_file, bucket, s3_file):
         print("Credentials not available")
         return False
 
-# Load the CSV files
-betting_odds_data = pd.read_csv('mlb_props.csv')
-prizepicks_data = pd.read_csv('test2.csv')
-
-
-prizepicks_data = prizepicks_data[(prizepicks_data['Prop'] == 'Total Bases') | 
-                                  (prizepicks_data['Prop'] == 'Pitcher Strikeouts') | 
-                                  (prizepicks_data['Prop'] == 'Runs')]
-prop_types= {
-    'Pitcher Strikeouts': 'Strikeouts',
-    'Runs': 'Runs',
-    'Total Bases': 'Total Bases'# example of making an indirect relationship
-    # Add other mappings as required
-}
-prizepicks_data['Prop'] = prizepicks_data['Prop'].map(prop_types)
-
-prop_types= {
-    'strikeouts': 'Strikeouts',
-    'runs': 'Runs', 
-    'bases': 'Total Bases'# example of making an indirect relationship
-    # Add other mappings as required
-}
-betting_odds_data['Prop'] = betting_odds_data['Prop'].map(prop_types)
-
-merged_data = pd.merge(betting_odds_data, prizepicks_data, left_on=['PlayerName', 'Prop'], right_on=['Name', 'Prop'], how='left')
-
-r = merged_data.drop(['Team', 'Name', 'Unnamed: 0'], axis =1)
-
-r =r.dropna(subset=['Value'])
-r = r.where(pd.notnull(r), "None")
-r=r.drop_duplicates()
-r.sort_values(by = 'fanduel', ascending=True, inplace = True)
-
-
-
-r.to_csv('merged_data.csv')
-upload_to_aws('merged_data.csv', BUCKET_NAME, 'merged_data.csv')
-
-
 def download_from_s3(bucket, s3_file, local_file):
     try:
         s3.download_file(bucket, s3_file, local_file)
@@ -75,15 +36,23 @@ def download_from_s3(bucket, s3_file, local_file):
     except NoCredentialsError:
         print("Credentials not available")
         return False
+
 def push_to_github():
     try:
         repo_dir = os.path.dirname(os.path.abspath(__file__))  # Get the current script directory
         os.chdir(repo_dir)
         print(f"Current directory: {os.getcwd()}")
+
+        # Initialize git repository if not found
+        if not os.path.exists(os.path.join(repo_dir, '.git')):
+            subprocess.check_call(['git', 'init'])
+            subprocess.check_call(['git', 'remote', 'add', 'origin', 'https://github.com/DhirenShivdasani/mlb.git'])
+
         subprocess.check_call(['git', 'config', '--global', 'user.email', 'dhiren3102@gmail.com'])
         subprocess.check_call(['git', 'config', '--global', 'user.name', 'DhirenShivdasani'])
         subprocess.check_call(['git', 'add', '.'])
         subprocess.check_call(['git', 'commit', '-m', 'Automated update by scheduler'])
+        
         # Use the token from environment variables for authentication
         github_token = os.getenv('GITHUB_TOKEN')
         subprocess.check_call([
@@ -92,6 +61,38 @@ def push_to_github():
         print("Changes pushed to GitHub")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while pushing to GitHub: {e}")
+
+# Load the CSV files
+betting_odds_data = pd.read_csv('mlb_props.csv')
+prizepicks_data = pd.read_csv('test2.csv')
+
+prizepicks_data = prizepicks_data[(prizepicks_data['Prop'] == 'Total Bases') | 
+                                  (prizepicks_data['Prop'] == 'Pitcher Strikeouts') | 
+                                  (prizepicks_data['Prop'] == 'Runs')]
+prop_types= {
+    'Pitcher Strikeouts': 'Strikeouts',
+    'Runs': 'Runs',
+    'Total Bases': 'Total Bases'
+}
+prizepicks_data['Prop'] = prizepicks_data['Prop'].map(prop_types)
+
+prop_types= {
+    'strikeouts': 'Strikeouts',
+    'runs': 'Runs', 
+    'bases': 'Total Bases'
+}
+betting_odds_data['Prop'] = betting_odds_data['Prop'].map(prop_types)
+
+merged_data = pd.merge(betting_odds_data, prizepicks_data, left_on=['PlayerName', 'Prop'], right_on=['Name', 'Prop'], how='left')
+
+r = merged_data.drop(['Team', 'Name', 'Unnamed: 0'], axis=1)
+r = r.dropna(subset=['Value'])
+r = r.where(pd.notnull(r), "None")
+r = r.drop_duplicates()
+r.sort_values(by='fanduel', ascending=True, inplace=True)
+
+r.to_csv('merged_data.csv')
+upload_to_aws('merged_data.csv', BUCKET_NAME, 'merged_data.csv')
 
 s3_file = 'merged_data.csv'
 local_file = 'merged_data.csv'
