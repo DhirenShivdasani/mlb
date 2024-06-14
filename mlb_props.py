a38ea38f1f08b56e14f396285ba34eed372aeff2
DhirenShivdasani <dhiren3102@gmail.com>
2024-06-14 16:00:20 -0400

    'echo'

--- mlb_props.py
+++ mlb_props.py
@@ -71,23 +71,8 @@ def push_to_github():
         subprocess.check_call(['git', 'config', '--global', 'user.email', 'dhiren3102@gmail.com'])
         subprocess.check_call(['git', 'config', '--global', 'user.name', 'DhirenShivdasani'])
 
-        # Print the file content before and after the update
-        with open('mlb_props.csv', 'r') as file:
-            print("Before update:\n", file.read())
-
-        # Manually delete and recreate the file to ensure changes are recognized
-        os.remove('mlb_props.csv')
-
-        # Create and write the updated data to the file
-        with open('mlb_props.csv', 'w') as file:
-            pivot_df.to_csv(file, index=False)
-
-        with open('mlb_props.csv', 'r') as file:
-            print("After update:\n", file.read())
-
         # Add and commit changes
-        subprocess.check_call(['git', 'add', '-A'])  # Force add all changes
-        subprocess.check_call(['git', 'status', '-v'])
+        subprocess.check_call(['git', 'add', '.'])
 
         # Check for changes before attempting to commit
         result = subprocess.run(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)
@@ -98,10 +83,13 @@ def push_to_github():
             print("No changes to commit")
 
         # Push changes to GitHub using the token for authentication
-        subprocess.check_call(['git', 'push', 'origin', 'main', '--verbose'])
+        env = os.environ.copy()
+        env['GIT_ASKPASS'] = os.path.abspath('echo-github-token.bat')
+        subprocess.check_call(['git', 'push', 'origin', 'main'], env=env)
         print("Changes pushed to GitHub")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while pushing to GitHub: {e}")
+
 url = 'https://www.rotowire.com/betting/mlb/player-props.php'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
