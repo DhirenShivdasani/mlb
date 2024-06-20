5145fe86b9167f9037f243a3f648b00ec8f7f7c5
DhirenShivdasani <dhiren3102@gmail.com>
2024-06-20 17:52:34 -0400

    '6/20'

--- pp_mlb.py
+++ pp_mlb.py
@@ -134,9 +134,10 @@ chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
 chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 chrome_options.add_argument("--window-size=1920x1080")  # Set window size for headless mode
 chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+
 if 'DYNO' in os.environ:
     chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
-    browser_executable_path = '/app/.apt/usr/bin/google-chrome'
+    browser_executable_path = '/app/.apt/usr/bin/chromedriver'
 else:
     chrome_options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'  # Adjust this path to your local Chrome binary location
     browser_executable_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'  # Adjust this path to your local Chrome binary location
