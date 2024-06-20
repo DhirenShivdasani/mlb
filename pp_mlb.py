dfe7a9552d1a7c51cac4c012fdf9c314a62d5545
DhirenShivdasani <dhiren3102@gmail.com>
2024-06-20 19:08:50 -0400

    'chrome'

--- pp_mlb.py
+++ pp_mlb.py
@@ -135,23 +135,25 @@ chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resou
 chrome_options.add_argument("--window-size=1920x1080")  # Set window size for headless mode
 chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
 
-if 'DYNO' in os.environ:
-    chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
-    browser_executable_path = '/app/.apt/usr/bin/google-chrome'
-else:
-    chrome_options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'  # Adjust this path to your local Chrome binary location
-    browser_executable_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'  # Adjust this path to your local Chrome binary location
-
 chrome_options.add_experimental_option("prefs", {
     "profile.default_content_setting_values.geolocation": 1, # 1:Allow, 2:Block
 })
 
 chrome_options.page_load_strategy = 'eager'  # Waits for the DOMContentLoaded event
 
+if 'DYNO' in os.environ:
+    chrome_options.binary_location = '/app/.apt/usr/bin/google-chrome'
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+else:
+    chrome_options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+    browser_executable_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
+
 print(f"Chrome binary location: {chrome_options.binary_location}")
+print(f"Chromedriver path: {browser_executable_path}")
 
-driver = uc.Chrome(options=chrome_options, browser_executable_path='/app/.apt/usr/bin/google-chrome')
-
+driver = uc.Chrome(options=chrome_options, browser_executable_path=browser_executable_path)
 
 driver.get("https://app.prizepicks.com/")
 # time.sleep(5)
