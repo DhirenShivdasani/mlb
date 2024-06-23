4f950999c8258a4cf2c37203c39d7437053b5ed4
DhirenShivdasani <dhiren3102@gmail.com>
2024-06-23 16:03:58 -0400

    'html'

--- pp_mlb.py
+++ pp_mlb.py
@@ -124,126 +124,120 @@ def push_to_github():
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while pushing to GitHub: {e.output.decode()}")
 
+chrome_options = uc.ChromeOptions()
+chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+chrome_options.add_argument("--disable-notifications")
+chrome_options = uc.ChromeOptions()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
+chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
+chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+chrome_options.add_argument("--window-size=1920x1080")  # Set window size for headless mode
+chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+
+chrome_options.add_experimental_option("prefs", {
+    "profile.default_content_setting_values.geolocation": 1, # 1:Allow, 2:Block
+})
+
+chrome_options.page_load_strategy = 'eager'  # Waits for the DOMContentLoaded event
+
 if 'DYNO' in os.environ:
-    node_command = 'node scrape.js'
-    process = subprocess.Popen(node_command, shell=True, stdout=subprocess.PIPE)
-    process.wait()
-    output, error = process.communicate()
-    if error:
-        print(f"Error: {error}")
-    else:
-        print(output.decode('utf-8'))
+    chrome_options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+    driver = uc.Chrome(options=chrome_options)
 else:
-    chrome_options = uc.ChromeOptions()
-    chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options = uc.ChromeOptions()
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode
-    chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
-    chrome_options.add_argument("--no-sandbox")  # Bypass OS security model
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-    chrome_options.add_argument("--window-size=1920x1080")  # Set window size for headless mode
-    chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
-
-    chrome_options.add_experimental_option("prefs", {
-        "profile.default_content_setting_values.geolocation": 1, # 1:Allow, 2:Block
-    })
-
-    chrome_options.page_load_strategy = 'eager'  # Waits for the DOMContentLoaded event
-
-   
+    chrome_options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
     driver = uc.Chrome(options=chrome_options, browser_executable_path='C:/Program Files/Google/Chrome/Application/chrome.exe')
 
-    print(f"Chrome binary location: {chrome_options.binary_location}")
-    print(f"Chromedriver path: {os.environ.get('CHROMEDRIVER_PATH')}")
-    driver.get("https://app.prizepicks.com/")
-    # time.sleep(5)
+print(f"Chrome binary location: {chrome_options.binary_location}")
+print(f"Chromedriver path: {os.environ.get('CHROMEDRIVER_PATH')}")
+driver.get("https://app.prizepicks.com/")
+# time.sleep(5)
 
-    wait = WebDriverWait(driver, timeout = 20)  # Wait for up to 10 seconds
-    try:
-        element = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[3]/div/div/button')))
-        element.click()
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-    ppPlayers = []
+wait = WebDriverWait(driver, timeout = 20)  # Wait for up to 10 seconds
+try:
+    element = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div[3]/div/div/button')))
+    element.click()
+except Exception as e:
+    print(f"Error: {e}")
 
 
-    wait = WebDriverWait(driver, 10)  # Wait for up to 10 seconds
-    try:
-        mlb_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='MLB']")))
-        mlb_button.click()
-    except Exception as e:
-        print(f"Error: {e}")
-
-    time.sleep(4)
+ppPlayers = []
 
 
-    try:
-        stat_container = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "stat-container"))
-        )
-    except TimeoutException:
-        print(driver.page_source)
-        raise
+wait = WebDriverWait(driver, 10)  # Wait for up to 10 seconds
+try:
+    mlb_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//span[text()='MLB']")))
+    mlb_button.click()
+except Exception as e:
+    print(f"Error: {e}")
+
+time.sleep(4)
 
 
-    categories = driver.find_element(By.CSS_SELECTOR, ".stat-container").text.split('\n')
-
-    for category in categories:
-        driver.find_element(By.XPATH, f"//div[text()='{category}']").click()
-
-
-        # projectionsPP = WebDriverWait(driver, 2).until(
-        #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, "/html/body/div[1]/div/div[3]/div[1]/div/main/div/div[2]")))
-        # print(projectionsPP)
-
-        WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, "#projections > ul"))
+try:
+    stat_container = WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, "stat-container"))
     )
+except TimeoutException:
+    print(driver.page_source)
+    raise
 
-        # Find all prop items under the specified container
-        projectionsPP = driver.find_elements(By.CSS_SELECTOR, "#projections > ul > li")
 
-        for projections in projectionsPP:
+categories = driver.find_element(By.CSS_SELECTOR, ".stat-container").text.split('\n')
 
-            goblin_icon = projections.find_elements(By.XPATH, ".//button[@aria-label='Open modal for Demons and Goblins']")
-            
-            # If the goblin icon is present, skip this prop
-            if goblin_icon:
-                continue
-            
+for category in categories:
+    driver.find_element(By.XPATH, f"//div[text()='{category}']").click()
 
-            names = projections.find_element(By.ID, "test-player-name").text
-            team = projections.find_element(By.ID, 'test-team-position').text
-            value = projections.find_element(By.CSS_SELECTOR, '.flex.flex-1.items-center.pr-2').text.strip()
-            proptype = projections.find_element(By.CSS_SELECTOR, 'div.align-items-center > div.text-soClean-140').text.strip()
-            
-            players = {
-                'Name': names,
-                'Team': team,
-                'Value': value,
-                'Prop': proptype.replace("<wbr>", "")
-            }
-            ppPlayers.append(players)
 
-    dfProps = pd.DataFrame(ppPlayers)
-    # CHANGE THE NAME OF THE FILE TO YOUR LIKING
-    dfProps.to_csv('test2.csv')
+    # projectionsPP = WebDriverWait(driver, 2).until(
+    #     EC.presence_of_all_elements_located((By.CSS_SELECTOR, "/html/body/div[1]/div/div[3]/div[1]/div/main/div/div[2]")))
+    # print(projectionsPP)
 
-    print("These are all of the props offered by PP.", '\n')
-    print(dfProps)
-    print('\n')
+    WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, "#projections > ul"))
+)
 
-    upload_to_aws('test2.csv', BUCKET_NAME, 'test2.csv')
+    # Find all prop items under the specified container
+    projectionsPP = driver.find_elements(By.CSS_SELECTOR, "#projections > ul > li")
 
-    s3_file = 'test2.csv'
-    local_file = 'test2.csv'
+    for projections in projectionsPP:
 
-    # Download the file from S3
-    if download_from_s3(BUCKET_NAME, s3_file, local_file):
-        push_to_github()
-    else:
-        print("Failed to download file from S3, not pushing to GitHub")
+        goblin_icon = projections.find_elements(By.XPATH, ".//button[@aria-label='Open modal for Demons and Goblins']")
+        
+        # If the goblin icon is present, skip this prop
+        if goblin_icon:
+            continue
+        
+
+        names = projections.find_element(By.ID, "test-player-name").text
+        team = projections.find_element(By.ID, 'test-team-position').text
+        value = projections.find_element(By.CSS_SELECTOR, '.flex.flex-1.items-center.pr-2').text.strip()
+        proptype = projections.find_element(By.CSS_SELECTOR, 'div.align-items-center > div.text-soClean-140').text.strip()
+        
+        players = {
+            'Name': names,
+            'Team': team,
+            'Value': value,
+            'Prop': proptype.replace("<wbr>", "")
+        }
+        ppPlayers.append(players)
+
+dfProps = pd.DataFrame(ppPlayers)
+# CHANGE THE NAME OF THE FILE TO YOUR LIKING
+dfProps.to_csv('test2.csv')
+
+print("These are all of the props offered by PP.", '\n')
+print(dfProps)
+print('\n')
+
+upload_to_aws('test2.csv', BUCKET_NAME, 'test2.csv')
+
+s3_file = 'test2.csv'
+local_file = 'test2.csv'
+
+# Download the file from S3
+if download_from_s3(BUCKET_NAME, s3_file, local_file):
+    push_to_github()
+else:
+    print("Failed to download file from S3, not pushing to GitHub")
 
