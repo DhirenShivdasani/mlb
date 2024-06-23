b64a1c577abcbe8e0953cc0bf1f76c568b92d254
milesjuddporter <milesjuddporter@gmail.com>
2024-06-23 19:47:52 -0400

    added react part with tailwind and daisyui

---
+++ mlb-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-3xl'>Miles</h1>
+      <button className='btn btn-primary'>Click me</button>
+    </div>
+  );
+}
+
+export default App;
