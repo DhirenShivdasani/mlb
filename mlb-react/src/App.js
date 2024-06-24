2d0b5856a27a8870b6cea317df5959c830a287b5
DhirenShivdasani <dhiren3102@gmail.com>
2024-06-23 20:00:27 -0400

    'miley'

--- mlb-react/src/App.js
+++ mlb-react/src/App.js
@@ -5,7 +5,21 @@ function App() {
   return (
     <div className="App">
       <h1 className='text-3xl'>Miles</h1>
-      <button className='btn btn-primary'>Click me</button>
+      <button className='btn btn-secondary btn-outline'>Click me</button>
+      <div className="card bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
     </div>
   );
 }
