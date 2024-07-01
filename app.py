42b06f77b022a976f707472ee71e7d86d7e9b8af
DhirenShivdasani <dhiren3102@gmail.com>
2024-06-30 21:40:07 -0400

    'react'

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ from flask_session import Session
 prod = False
 DATABASE_URL = os.getenv('DATABASE_URL')
 # DATABASE_URL ='postgres://u6aoo300n98jv9:p5b0f8d8acf4792b0bfd49cb4f620561db87f220ced59f5ad9d729ddda6cbfc97@cd5gks8n4kb20g.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/doo2eame5lshp'
-cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # Secret key for sessions
