d98daa3985d8f06d28ac1767b41501612f378043
DhirenShivdasani <dhiren3102@gmail.com>
2024-07-01 22:26:32 -0400

    react

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import queue
 import threading
 import asyncio
 import json
-from flask_talisman import Talisman
 
 prod = False
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -17,7 +16,6 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 app = Flask(__name__)
 app.secret_key = os.urandom(24)  # Secret key for sessions
 CORS(app)
-Talisman(app)
 
 
 app.config['SESSION_TYPE'] = 'filesystem'
